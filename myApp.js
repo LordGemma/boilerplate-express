a26701e8e4d5f985908edcc39f9b8d7612fc66bb
Vadym Aminov <aminov.v.o@gmail.com>
2021-01-27 18:28:36 +0000

    add multiple routes

--- myApp.js
+++ myApp.js
@@ -1,39 +1,15 @@
-var express = require('express');
-var app = express();
+const path = require("path");
+const express = require('express');
+const app = express();
 
+app.use(express.static(__dirname + "/public"));
 
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, 'views/index.html'));
+})
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.get("/json", (req, res) => {
+  res.json({"message": "Hello json"});
+})
 
  module.exports = app;
