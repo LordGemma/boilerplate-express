72836e73e7fa0f890c30f3d1c4bd2be5a2d2087b
Vadym Aminov <aminov.v.o@gmail.com>
2021-01-28 07:57:09 +0000

    add middleware for /json route

--- myApp.js
+++ myApp.js
@@ -3,6 +3,10 @@ const express = require('express');
 const app = express();
 
 app.use(express.static(__dirname + "/public"));
+app.use('/json', (req, res, next) => {
+  console.log(`${req.method} ${req.path} - ${req.ip}`)
+  next();
+})
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, 'views/index.html'));
